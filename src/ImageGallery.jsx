d9dffbc48896acc8ad784a2eee1386acc80ef38c
Anthony Spence <anthonyspence08091973@gmail.com>
2023-10-19 09:16:56 -0700

    newslist and imagegallery components

---
+++ src/ImageGallery.jsx
@@ -0,0 +1,42 @@
+// src/components/ImageGallery.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ImageGallery({ query }) {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const apiKey = 'PEXELS_API_KEY';
+    const apiUrl = `https://api.pexels.com/v1/search?query=${query}&per_page=10`;
+
+    axios
+      .get(apiUrl, {
+        headers: {
+          Authorization: 'PEXELS_API_KEY',
+        },
+      })
+      .then((response) => {
+        setImages(response.data.photos);
+      })
+      .catch((error) => {
+        console.error('Error fetching images:', error);
+      });
+  }, [query]);
+
+  return (
+    <div>
+      <h2>Image Gallery</h2>
+      <div className="image-grid">
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.src.medium}
+            alt={image.photographer}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ImageGallery;
---
+++ src/NewsList.jsx
@@ -0,0 +1,38 @@
+// src/components/NewsList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function NewsList({ query }) {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const apiKey = 'YOUR_NEWS_API_KEY';
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setArticles(response.data.articles);
+      })
+      .catch((error) => {
+        console.error('Error fetching news data:', error);
+      });
+  }, [query]);
+
+  return (
+    <div>
+      <h2>News Articles</h2>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.url}>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              {article.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default NewsList;
